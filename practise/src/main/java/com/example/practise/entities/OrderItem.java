1b7152875558212fc2d058cd0ecd92ee737fc2da
Rishabh Singh <20rishabh05@gmail.com>
2026-05-20 13:14:44 +0530

    feat: add relationships between OrderItem and Product, Order entities

--- practise/src/main/java/com/example/practise/entities/OrderItem.java
+++ practise/src/main/java/com/example/practise/entities/OrderItem.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -36,5 +37,10 @@ public class OrderItem {
 
     // some relations
     @ManyToOne
+    @JoinColumn(name = "order_id", nullable = false)
     private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
 }
--- practise/src/main/java/com/example/practise/entities/Product.java
+++ practise/src/main/java/com/example/practise/entities/Product.java
@@ -1,10 +1,15 @@
 package com.example.practise.entities;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Min;
@@ -44,4 +49,10 @@ public class Product {
     @Min(value = 0, message = "Stock cannot be less than zero")
     @Column(name = "stock_quantity", nullable = false)
     private Integer stockQuantity;
+
+    // todo relations
+    @JsonIgnore
+    @OneToMany(mappedBy = "product")
+    private List<OrderItem> orderItems;
+
 }
