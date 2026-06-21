ef9a95b27d428b03dba9f344c53b57f644fa0324
Rishabh Singh <20rishabh05@gmail.com>
2026-06-21 21:53:54 +0530

    feat: enhance Student entity with Builder pattern and implement createStudent method in StudentServiceImpl

--- revise/src/main/java/com/rishabh/revise/service/StudentServiceImpl.java
+++ revise/src/main/java/com/rishabh/revise/service/StudentServiceImpl.java
@@ -2,35 +2,52 @@ package com.rishabh.revise.service;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import com.rishabh.revise.repository.StudentRepository;
+
+import lombok.RequiredArgsConstructor;
+
 import com.rishabh.revise.dto.StdRequest;
 import com.rishabh.revise.dto.StdResponse;
+import com.rishabh.revise.entity.Student;
+
+@Service
+@RequiredArgsConstructor
 
 public class StudentServiceImpl implements StudentService {
+    private final StudentRepository studentRepository;
 
     @Override
     public StdResponse createStudent(StdRequest stdRequest) {
+        Student student = Student.builder()
+                .name(stdRequest.getName())
+                .email(stdRequest.getEmail())
+                .password(stdRequest.getPassword())
+                .build();
 
-    }
+        studentRepository.save(student);
+        return StdResponse.builder()
+                .name(student.getName())
+                .mail(student.getEmail())
+                .build();
+}
 
-    
     @Override
     public List<StdResponse> getAllStudents() {
 
     }
 
-    
     @Override
     public StdResponse getStudentById(Long id) {
 
     }
 
-    
     @Override
-    public StdResponse updateStudent(Long id,  StdRequest request) {
+    public StdResponse updateStudent(Long id, StdRequest request) {
 
     }
 
-    
     @Override
     public void deleteStudent(Long id) {
 
