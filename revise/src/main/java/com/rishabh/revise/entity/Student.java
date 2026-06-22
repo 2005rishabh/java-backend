f326434f2628625f21a4e004977783044f89165c
Rishabh Singh <20rishabh05@gmail.com>
2026-06-23 00:27:03 +0530

    fix: add missing @GeneratedValue annotation for id field in Student entity

--- revise/src/main/java/com/rishabh/revise/entity/Student.java
+++ revise/src/main/java/com/rishabh/revise/entity/Student.java
@@ -2,6 +2,8 @@ package com.rishabh.revise.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -19,6 +21,7 @@ import lombok.Setter;
 public class Student {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
     @Column(name = "student_name", nullable = false)
