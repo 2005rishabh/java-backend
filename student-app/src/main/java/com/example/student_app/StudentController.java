b4d57361ce8b25b91a8a565c5ea1f2119ea3be83
Rishabh Singh <20rishabh05@gmail.com>
2026-06-29 11:26:43 +0530

    feat: initialize student application with Maven and Spring Boot setup

---
+++ student-app/src/main/java/com/example/student_app/StudentController.java
@@ -0,0 +1,18 @@
+package com.example.student_app;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class StudentController {
+    List<Student> getStudents() {
+        return List.of(
+            new Student(1, "rishabh", 21),
+            new Student(2, "shivek", 21),
+            new Student(3, "shobhit", 21),
+            new Student(4, "sahil", 21)
+        );
+    }
+}
