34c43b4404efd1625e34bd9d645e88ffb3bf1dba
Rishabh Singh <20rishabh05@gmail.com>
2026-06-29 11:33:38 +0530

    feat: implement StudentRepo interface for database operations and update StudentController formatting

--- student-app/src/main/java/com/example/student_app/StudentController.java
+++ student-app/src/main/java/com/example/student_app/StudentController.java
@@ -2,17 +2,19 @@ package com.example.student_app;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
 public class StudentController {
+
+    @RequestMapping("/")
     List<Student> getStudents() {
         return List.of(
-            new Student(1, "rishabh", 21),
-            new Student(2, "shivek", 21),
-            new Student(3, "shobhit", 21),
-            new Student(4, "sahil", 21)
-        );
+                new Student(1, "rishabh", 21),
+                new Student(2, "shivek", 21),
+                new Student(3, "shobhit", 21),
+                new Student(4, "sahil", 21));
     }
 }
