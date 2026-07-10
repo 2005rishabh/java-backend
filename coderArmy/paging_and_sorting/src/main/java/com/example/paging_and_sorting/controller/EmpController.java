d2b0f54663df1f039b88fe64fe7c6ab9ee2477fe
Rishabh Singh <20rishabh05@gmail.com>
2026-07-10 12:03:49 +0530

    feat: add pagination support in Employee retrieval with page number and size parameters

--- coderArmy/paging_and_sorting/src/main/java/com/example/paging_and_sorting/controller/EmpController.java
+++ coderArmy/paging_and_sorting/src/main/java/com/example/paging_and_sorting/controller/EmpController.java
@@ -1,7 +1,10 @@
 package com.example.paging_and_sorting.controller;
 
 import java.util.List;
+
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.paging_and_sorting.model.Employee;
@@ -20,8 +23,8 @@ public class EmpController {
 
     @GetMapping("/list")
     public List<Employee> getEmployees(@RequestBody int pageNo, 
-                                    @
+                                    @RequestBody int pageSize
     ) {
-        return empService.fetchEmployees();
+        return empService.fetchEmployees(PageRequest.of(pageNo, pageSize));
     }
 }
