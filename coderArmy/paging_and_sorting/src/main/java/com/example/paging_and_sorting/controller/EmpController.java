daee4c1fd01d488056ccc55229c669333c8698df
Rishabh Singh <20rishabh05@gmail.com>
2026-07-10 12:08:14 +0530

    feat: update Employee retrieval to use @RequestParam for pagination parameters

--- coderArmy/paging_and_sorting/src/main/java/com/example/paging_and_sorting/controller/EmpController.java
+++ coderArmy/paging_and_sorting/src/main/java/com/example/paging_and_sorting/controller/EmpController.java
@@ -1,11 +1,10 @@
 package com.example.paging_and_sorting.controller;
 
 import java.util.List;
-
 import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.paging_and_sorting.model.Employee;
 import com.example.paging_and_sorting.service.EmpService;
@@ -16,15 +15,16 @@ public class EmpController {
 
     private final EmpService empService;
 
-    // Spring will automatically inject the EmpService bean here
     public EmpController(EmpService empService) {
         this.empService = empService;
     }
 
     @GetMapping("/list")
-    public List<Employee> getEmployees(@RequestBody int pageNo, 
-                                    @RequestBody int pageSize
-    ) {
-        return empService.fetchEmployees(PageRequest.of(pageNo, pageSize));
+    public List<Employee> getEmployees(
+            @RequestParam(defaultValue = "1") int pageNo,
+            @RequestParam(defaultValue = "10") int pageSize) {
+        int pageIndex = (pageNo < 1) ? 0 : pageNo - 1;
+
+        return empService.fetchEmployees(PageRequest.of(pageIndex, pageSize));
     }
 }
